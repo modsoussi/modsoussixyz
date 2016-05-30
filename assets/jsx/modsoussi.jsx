8afc1c1ab4a082a5eae2d396179a51f3abc47c42
modsoussi <mohameddsoussi@gmail.com>
2016-05-30 17:55:57 -0400

    dependencies, again

---
+++ assets/jsx/modsoussi.jsx
@@ -0,0 +1,12 @@
+
+var Nav = React.creatClass({
+	render: function(){
+		return(
+			<nav class="navbar navbar-default">
+				<ul class="nav nav-tabs">
+					<li class="nav-item active">Hello, World!</li>
+				</ul>
+			</nav>
+		);
+	}
+});
