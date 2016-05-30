c0c23b95699c9d63daf605222d8fbf515cb0fec7
modsoussi <mohameddsoussi@gmail.com>
2016-05-30 19:01:15 -0400

    it works, no need for webpack

--- assets/jsx/modsoussi.jsx
+++ assets/jsx/modsoussi.jsx
@@ -1,12 +1,30 @@
+/**
+ * Main JSX file.
+ * 
+ * (c) 2016. modsoussi.
+ */
 
-var Nav = React.creatClass({
+var Nav = React.createClass({
 	render: function(){
 		return(
-			<nav class="navbar navbar-default">
-				<ul class="nav nav-tabs">
-					<li class="nav-item active">Hello, World!</li>
+			<nav className="navbar navbar-light" style={{margin: 15, borderBottom: 2}}>
+				<ul className="nav navbar-nav">
+					<li className="nav-item active" href="#">modsoussi</li>
+				</ul>
+				
+				<ul className="nav navbar-nav navbar-right">
+					<li className="nav-item active" href="#">
+						<g>
+							<i className="fa fa-facebook fa"></i>
+						</g>
+					</li>
 				</ul>
 			</nav>
 		);
 	}
 });
+
+ReactDOM.render(
+	<Nav />,
+	document.getElementById("main")
+);
