42431aa847c0fb955a29f17b490303cf4821d4ff
modsoussi <mohameddsoussi@gmail.com>
2016-06-14 20:38:46 -0400

    added home content

--- assets/jsx/modsoussi.jsx
+++ assets/jsx/modsoussi.jsx
@@ -9,8 +9,8 @@ var Nav = React.createClass({
 		return(
 			<nav id="b-override" className="navbar navbar-light" style={{marginTop: 15, marginLeft: 25, marginRight: 25, fontSize: 20}}>
 				<ul className="nav navbar-nav">
-					<li className="nav-item inactive"><a href="#" style={{fontSize: 36}}>modsoussi</a></li>
-					<li className="nav-item active"><a href="#" style={{marginLeft: 25, marginTop: 5}}>about</a></li>
+					<li className="nav-item inactive"><a style={{fontSize: 36}}>modsoussi</a></li>
+					<li className="nav-item active"><a href="#" style={{marginLeft: 25, marginTop: 5}}>skillz</a></li>
 					<li className="nav-item active"><a href="#" style={{marginTop: 5}}>projects</a></li>
 				</ul>
 				
@@ -49,12 +49,54 @@ var Nav = React.createClass({
 	}
 });
 
+var Content = React.createClass({
+	getInitialState: function(){
+		return {tab: this.props.tab};
+	},
+	render: function(){
+		if(this.state.tab === "home"){
+			return(
+				<div className="container" style={{marginTop: 50, fontSize: 25}}>
+					<div className="row">
+						<div className="col-md-6 center-block">
+							<p>Hi there, friend! My name is Mohamed, and I go by Mo. I was born
+								and raised in Bizerte, Tunisia, before life led me to a wonderful piece of
+								land in western Massachussetts that people call the Purple Bubble, and there stood
+								the top liberal arts college in the country, Williams College. In this bubble in the
+								middle of the Berkshires, I majored in Physics and Computer Science and set out to change
+								the world. My belief is that humanity's future lies in its expansion outside of planet Earth.
+								I'm only 24, and I'm going to do everything I can to see it happen in my lifetime. @elonmusk ;)
+							 </p>
+						</div>
+					</div>
+				</div>
+			);
+		} else if(this.state.tab === "about"){
+			return(
+				<div className="container">
+					<div className="row">
+						<div className="col-md-6 center-block">
+							<h2> Hello, about! </h2>
+						</div>
+					</div>
+				</div>
+			);
+		} else return(
+				<div className="container">
+					<div className="row">
+						<div className="col-md-6 center-block">
+							<h2> Hello, projects! </h2>
+						</div>
+					</div>
+				</div>
+		);
+	}
+})
+
 var Body = React.createClass({
 	render: function(){
 		return(
-			<div className="container" style={{margin: 50}}>
-				
-			</div>
+			<Content tab="home"/>
 		);
 	}
 });
