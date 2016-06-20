788824eeae3947ae914ec6fe82eda9f07c770674
modsoussi <mohameddsoussi@gmail.com>
2016-06-20 03:28:12 +0000

    responsiveness in progress

--- assets/jsx/modsoussi.jsx
+++ assets/jsx/modsoussi.jsx
@@ -8,7 +8,7 @@
 
 var React = require('react');
 var ReactDOM = require('react-dom');
-var ReactTransitionGroup = require('react-addons-transition-group');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 var Nav = React.createClass({
 	handleClick: function(e){
@@ -16,7 +16,7 @@ var Nav = React.createClass({
 	},
 	render: function(){
 		return(
-			<nav id="b-override" className="navbar navbar-light" style={{marginTop: 15, marginLeft: 25, marginRight: 25, fontSize: 20}}>
+			<nav className="navbar navbar-light" style={{marginTop: 15, marginLeft: 25, marginRight: 25, fontSize: 20}}>
 				<ul className="nav navbar-nav">
 					<li className="nav-item active">
 						<a href="#" style={{fontSize: 36}} onClick={this.handleClick}>
@@ -35,6 +35,86 @@ var Nav = React.createClass({
 					</li>
 				</ul>
 				
+				<ul className="nav navbar-nav navbar-right hidden-xs">
+					<li className="nav-item active">
+						<a target="_blank" href="https://twitter.com/modsoussi">
+							<g>
+								<i className="fa fa-twitter"></i>
+							</g>
+						</a>
+					</li>
+					<li className="nav-item active">
+						<a target="_blank" href="https://github.com/modsoussi">
+							<g>
+								<i className="fa fa-github"></i>
+							</g>
+						</a>
+					</li>
+					<li className="nav-item active">
+						<a target="_blank" href="https://linkedin.com/in/mohameddsoussi">
+							<g>
+								<i className="fa fa-linkedin"></i>
+							</g>
+						</a>
+					</li>
+					<li className="nav-item active">
+						<a href="#">
+							<g>
+								<i className="fa fa-envelope" style={{marginRight: 15}}></i>
+							</g>
+						</a>
+					</li>
+				</ul>
+			</nav>
+		);
+	}
+});
+
+var Content = React.createClass({
+	render: function(){
+		if(this.props.tab === "modsoussi"){
+			return(
+				<p>Hello there, friend! My name is Mohamed, and I go by Mo. I was born
+				and raised in Bizerte, Tunisia, before life led me to a wonderful piece of
+				land in western Massachussetts that people call the Purple Bubble, and there stood
+				the top liberal arts college in the country, Williams College. In this bubble in the
+				middle of the Berkshires, I majored in Physics and Computer Science and set out to change
+				the world. My belief is that Humanity's future lies in its expansion outside of planet Earth.
+				I'm only 24, and I'm going to do everything I can to see it happen in my lifetime. @elonmusk ;)
+				For the time being I'm with <a href="http://daycationapp.com" target="_blank">
+				Daycation</a>, all while I work on my aerospace engineering skills.
+				</p>
+			);
+		} else if(this.props.tab === "skillz"){
+			return(
+				<h2> Hello, skillz! </h2>
+			);
+		} else return(
+			<h2> Hello, projects! </h2>
+		);
+	}
+});
+
+var Body = React.createClass({
+	render: function(){
+		return(
+			<ReactCSSTransitionGroup transitionName="jone" transitionEnterTimeout={500} transitionLeaveTimeout={0.01}>
+				<div  key={this.props.tab} className="container">
+					<div className="row">
+						<div className="col-md-6 center-block">
+							<Content tab={this.props.tab}/>
+						</div>
+					</div>
+				</div>
+			</ReactCSSTransitionGroup>
+		)
+	}
+});
+
+var Footer = React.createClass({
+	render: function() {
+		return(
+			<nav className="navbar navbar-light navbar-static-bottom visible-xs" style={{margin: 10}}>
 				<ul className="nav navbar-nav navbar-right">
 					<li className="nav-item active">
 						<a target="_blank" href="https://twitter.com/modsoussi">
@@ -70,49 +150,6 @@ var Nav = React.createClass({
 	}
 });
 
-var Body = React.createClass({
-	render: function(){
-		if(this.props.tab === "modsoussi"){
-			return(
-				<div className="container" style={{marginTop: 30, fontSize: 25}}>
-					<div className="row">
-						<div className="col-md-6 center-block">
-							<p>Hello there, friend! My name is Mohamed, and I go by Mo. I was born
-								and raised in Bizerte, Tunisia, before life led me to a wonderful piece of
-								land in western Massachussetts that people call the Purple Bubble, and there stood
-								the top liberal arts college in the country, Williams College. In this bubble in the
-								middle of the Berkshires, I majored in Physics and Computer Science and set out to change
-								the world. My belief is that Humanity's future lies in its expansion outside of planet Earth.
-								I'm only 24, and I'm going to do everything I can to see it happen in my lifetime. @elonmusk ;)
-								For the time being, I'm working on <a href="http://daycationapp.com" target="_blank">
-								Daycation</a>, while I improve my software and aerospace engineering skills.
-							 </p>
-						</div>
-					</div>
-				</div>
-			);
-		} else if(this.props.tab === "skillz"){
-			return(
-				<div className="container">
-					<div className="row">
-						<div className="col-md-6 center-block">
-							<h2> Hello, skillz! </h2>
-						</div>
-					</div>
-				</div>
-			);
-		} else return(
-			<div className="container">
-				<div className="row">
-					<div className="col-md-6 center-block">
-						<h2> Hello, projects! </h2>
-					</div>
-				</div>
-			</div>
-		);
-	}
-});
-
 var Container = React.createClass({
 	getInitialState: function(){
 		return {tab: "modsoussi"};
@@ -125,6 +162,7 @@ var Container = React.createClass({
 			<div>
 				<Nav handleClick={this.handleClick}/>
 				<Body tab={this.state.tab}/>
+				<Footer />
 			</div>
 		);
 	}
